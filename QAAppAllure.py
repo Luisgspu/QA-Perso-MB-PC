f4e2f02e67d22d0528ee205814d5280639eff8dc
Mandalore136 <luis.gsepulveda136@gmail.com>
2025-05-03 22:26:21 +0200

    All Models All Markets LC Started

--- QAAppAllure.py
+++ QAAppAllure.py
@@ -178,24 +178,28 @@ def run_test(driver, test_name, market_code, model_code, model_name, body_type, 
 # Manually defined test cases
 manual_test_cases = [
     
-
-    
-    {"test_name": "Last Configuration Started",
-     "market_code": "IT/it",
-        "model_code": "C236",
-    },
-    
-    
-    
-    
-    
-  
-    
-
-
-  
-
-
+    {"test_name": "Last Configuration Started", "market_code": "AT/de"},
+    {"test_name": "Last Configuration Started", "market_code": "BE/nl"},
+    {"test_name": "Last Configuration Started", "market_code": "BE/fr"},
+    {"test_name": "Last Configuration Started", "market_code": "CH/de"},
+    {"test_name": "Last Configuration Started", "market_code": "CH/fr"},
+    {"test_name": "Last Configuration Started", "market_code": "CH/it"},
+    {"test_name": "Last Configuration Started", "market_code": "GB/en"},
+    {"test_name": "Last Configuration Started", "market_code": "CZ/cs"},
+    {"test_name": "Last Configuration Started", "market_code": "DE/de"},
+    {"test_name": "Last Configuration Started", "market_code": "DK/da"},
+    {"test_name": "Last Configuration Started", "market_code": "ES/es"},
+    {"test_name": "Last Configuration Started", "market_code": "FR/fr"},
+    {"test_name": "Last Configuration Started", "market_code": "HU/hu"},
+    {"test_name": "Last Configuration Started", "market_code": "IT/it"},
+    {"test_name": "Last Configuration Started", "market_code": "LU/de"},
+    {"test_name": "Last Configuration Started", "market_code": "LU/fr"},
+    {"test_name": "Last Configuration Started", "market_code": "NL/nl"},
+    {"test_name": "Last Configuration Started", "market_code": "PL/pl"},
+    {"test_name": "Last Configuration Started", "market_code": "PT/pt"},
+    {"test_name": "Last Configuration Started", "market_code": "RO/ro"},
+    {"test_name": "Last Configuration Started", "market_code": "SE/sv"},
+    {"test_name": "Last Configuration Started", "market_code": "SK/sk"}
     
     
 ]
