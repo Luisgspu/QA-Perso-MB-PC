62124c546acdfc582ab61314571f26a285f9a889
Luis Sepulveda <luis.sepulveda+omcitcld@credera.com>
2025-06-06 11:24:23 +0200

    Solved headless bug

--- App/CreateDriver.py
+++ App/CreateDriver.py
@@ -6,14 +6,15 @@ import time
 
 def build_chrome_options():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new") # Use new headless mode for better performance  
+    options.add_argument("--headless") # Use new headless mode for better performance  
     options.add_argument("--disable-gpu") 
     options.add_argument("--enable-webgl")
     options.add_argument("--incognito")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--maximize")
-    options.add_argument("--start-fullscreen")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--window-size=2560,1440")
     options.add_argument("--no-sandbox")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
 
